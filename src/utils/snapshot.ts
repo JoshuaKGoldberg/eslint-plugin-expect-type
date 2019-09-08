b42c84208bcfa035295a172dd3c32756a102c28d
Igor Bezkrovnyi <1188919+ibezkrovnyi@users.noreply.github.com>
2019-09-08 18:08:03 +0200

    ExpectTypeSnapshot (#2)

---
+++ src/utils/snapshot.ts
@@ -0,0 +1,33 @@
+import { dirname, resolve, basename } from 'path';
+import { ensureFileSync, readJsonSync, writeJsonSync } from 'fs-extra';
+
+export const getTypeSnapshot = (filename: string, snapshotName: string) => {
+  const snapshotPath = getSnapshotPath(filename);
+  const json = readJsonSync(snapshotPath, { throws: false });
+  if (!json) {
+    return;
+  }
+  return json[snapshotName] as string;
+};
+
+export const updateTypeSnapshot = (
+  filename: string,
+  snapshotName: string,
+  actualType: string,
+) => {
+  const snapshotPath = getSnapshotPath(filename);
+  ensureFileSync(snapshotPath)
+
+  const json = readJsonSync(snapshotPath, { throws: false }) || {};
+  json[snapshotName] = actualType;
+  writeJsonSync(snapshotPath, json);
+};
+
+function getSnapshotPath(filename: string) {
+  const directory = dirname(filename);
+  return resolve(
+    directory,
+    '__type-snapshots__',
+    `${basename(filename)}.snap.json`,
+  );
+}
