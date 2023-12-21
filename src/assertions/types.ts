740cd7814cd06a9924df4e0087cf0c79fb3cb27b
Josh Goldberg ✨ <git@joshuakgoldberg.com>
2023-12-20 20:24:18 -0500

    chore: split functionality internally (#117)
    ## PR Checklist
    
    - [x] Addresses an existing open issue: fixes #116
    - [x] That issue was marked as [`status: accepting
    prs`](https://github.com/JoshuaKGoldberg/eslint-plugin-expect-type/issues?q=is%3Aopen+is%3Aissue+label%3A%22status%3A+accepting+prs%22)
    - [x] Steps in
    [CONTRIBUTING.md](https://github.com/JoshuaKGoldberg/eslint-plugin-expect-type/blob/main/.github/CONTRIBUTING.md)
    were taken
    
    ## Overview
    
    Splits out the roughly-not-specific-to-ESLint logic from the `expect`
    rule into:
    
    * `src/assertions`: parsing assertions out from source code
    * `src/failures`: determined unmatched/unmet assertions
    * `src/utils`: miscellaneous helpers

---
+++ src/assertions/types.ts
@@ -0,0 +1,76 @@
+export interface SyntaxError {
+	readonly line: number;
+	readonly type:
+		| "InvalidTwoslash"
+		| "MissingExpectType"
+		| "MissingSnapshotName";
+}
+
+export interface ManualAssertion {
+	readonly assertionType: "manual";
+	readonly expected: string;
+}
+
+export interface SnapshotAssertion {
+	readonly assertionType: "snapshot";
+	readonly expected: string | undefined;
+	readonly snapshotName: string;
+}
+
+export interface TwoSlashAssertion {
+	readonly assertionType: "twoslash";
+
+	/**
+	 * The expected type in the twoslash comment
+	 */
+	readonly expected: string;
+
+	/**
+	 * Text before the "^?" (used to produce continuation lines for fixer)
+	 */
+	readonly expectedPrefix: string;
+
+	/**
+	 * Range of positions corresponding to the "expected" string (for fixer)
+	 */
+	readonly expectedRange: [number, number];
+
+	/**
+	 * Does a space need to be added after "^?" when fixing? (If "^?" ends the line.)
+	 */
+	readonly insertSpace: boolean;
+
+	/**
+	 * Position in the source file that the twoslash assertion points at
+	 */
+	readonly position: number;
+}
+
+export type Assertion = ManualAssertion | SnapshotAssertion | TwoSlashAssertion;
+
+export interface Assertions {
+	/**
+	 * Lines with more than one assertion (these are errors).
+	 */
+	readonly duplicates: readonly number[];
+
+	/**
+	 * Lines with an $ExpectError.
+	 */
+	readonly errorLines: ReadonlySet<number>;
+
+	/**
+	 * Syntax Errors
+	 */
+	readonly syntaxErrors: readonly SyntaxError[];
+
+	/**
+	 * Twoslash-style type assertions in the file
+	 */
+	readonly twoSlashAssertions: readonly TwoSlashAssertion[];
+
+	/**
+	 * Map from a line number to the expected type at that line.
+	 */
+	readonly typeAssertions: Map<number, Assertion>;
+}
---
+++ src/failures/getExpectTypeFailures.ts
@@ -0,0 +1,105 @@
+import ts from "typescript";
+
+import { Assertions } from "../assertions/types.js";
+import { getNodeAtPosition } from "../utils/locations.js";
+import { lineOfPosition } from "../utils/locations.js";
+import {
+	getLanguageServiceHost,
+	getNodeForExpectType,
+	matchModuloWhitespace,
+	matchReadonlyArray,
+} from "../utils/typescript.js";
+import { ExpectTypeFailures, UnmetExpectation } from "./types.js";
+
+export function getExpectTypeFailures(
+	sourceFile: ts.SourceFile,
+	assertions: Pick<Assertions, "twoSlashAssertions" | "typeAssertions">,
+	program: ts.Program,
+): ExpectTypeFailures {
+	const checker = program.getTypeChecker();
+	const languageService = ts.createLanguageService(
+		getLanguageServiceHost(program),
+	);
+	const { twoSlashAssertions, typeAssertions } = assertions;
+	const unmetExpectations: UnmetExpectation[] = [];
+
+	// Match assertions to the first node that appears on the line they apply to.
+	ts.forEachChild(sourceFile, function iterate(node) {
+		const line = lineOfPosition(node.getStart(sourceFile), sourceFile);
+		const assertion = typeAssertions.get(line);
+		if (assertion !== undefined) {
+			const { expected } = assertion;
+
+			let nodeToCheck = node;
+
+			// https://github.com/Microsoft/TypeScript/issues/14077
+			if (node.kind === ts.SyntaxKind.ExpressionStatement) {
+				node = (node as ts.ExpressionStatement).expression;
+			}
+
+			nodeToCheck = getNodeForExpectType(node);
+			const type = checker.getTypeAtLocation(nodeToCheck);
+			const actual = checker.typeToString(
+				type,
+				/*enclosingDeclaration*/ undefined,
+				ts.TypeFormatFlags.NoTruncation,
+			);
+
+			if (
+				!expected ||
+				(actual !== expected && !matchReadonlyArray(actual, expected))
+			) {
+				unmetExpectations.push({ actual, assertion, node });
+			}
+
+			typeAssertions.delete(line);
+		}
+
+		ts.forEachChild(node, iterate);
+	});
+
+	const twoSlashFailureLines: number[] = [];
+	if (twoSlashAssertions.length) {
+		for (const assertion of twoSlashAssertions) {
+			const { expected, position } = assertion;
+			if (position === -1) {
+				// special case for a twoslash assertion on line 1.
+				twoSlashFailureLines.push(0);
+				continue;
+			}
+
+			const node = getNodeAtPosition(sourceFile, position);
+			if (!node) {
+				twoSlashFailureLines.push(
+					sourceFile.getLineAndCharacterOfPosition(position).line,
+				);
+				continue;
+			}
+
+			const qi = languageService.getQuickInfoAtPosition(
+				sourceFile.fileName,
+				node.getStart(),
+			);
+			if (!qi?.displayParts) {
+				twoSlashFailureLines.push(
+					sourceFile.getLineAndCharacterOfPosition(position).line,
+				);
+				continue;
+			}
+
+			const actual = qi.displayParts.map((dp) => dp.text).join("");
+			if (!matchModuloWhitespace(actual, expected)) {
+				unmetExpectations.push({
+					actual,
+					assertion,
+					node,
+				});
+			}
+		}
+	}
+
+	return {
+		unmetExpectations,
+		unusedAssertions: [...twoSlashFailureLines, ...typeAssertions.keys()],
+	};
+}
