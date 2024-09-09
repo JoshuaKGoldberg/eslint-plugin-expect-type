81ea6dde1dd4583b585582a0436702595856647d
Josh Goldberg <git@joshuakgoldberg.com>
2024-09-09 15:48:45 -0400

    fix: bump ESLint-related packages to latest

--- src/utils/createRule.ts
+++ src/utils/createRule.ts
@@ -1,6 +1,8 @@
 import { ESLintUtils } from "@typescript-eslint/utils";
 
-export const createRule = ESLintUtils.RuleCreator(
+export const createRule = ESLintUtils.RuleCreator<{
+	requiresTypeChecking?: boolean;
+}>(
 	(name) =>
 		`https://github.com/JoshuaKGoldberg/eslint-plugin-expect-type/blob/main/docs/rules/${name}.md`,
 );
