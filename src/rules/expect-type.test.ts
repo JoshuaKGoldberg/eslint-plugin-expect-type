10131e127902eada64838736b0905a30d3336514
Josh Goldberg <git@joshuakgoldberg.com>
2024-11-28 12:52:12 -0500

    More comprehensive test coverage

--- src/rules/expect-type.test.ts
+++ src/rules/expect-type.test.ts
@@ -9,7 +9,7 @@ ruleTester.run("expect", expect, {
 			code: dedent`
       // $ExpectType number
       const t = 'a';
-    `,
+np    `,
 			errors: [
 				{
 					column: 1,
@@ -64,23 +64,86 @@ ruleTester.run("expect", expect, {
 		},
 	],
 	valid: [
-		// Primitive type
 		{
 			code: dedent`
       // $ExpectType number
       const t = 6 as number;
     `,
 			filename,
+			name: "Primitive type",
 		},
-		// Complex type
+		{
+			code: dedent`
+      // $ExpectType number
+      const t = 6 as (number);
+    `,
+			filename,
+			name: "Parenthesized primitive type",
+		},
+		{
+			code: dedent`
+      // $ExpectType number | string
+      const t = 6 as number | string;
+    `,
+			filename,
+			name: "Union of primitive types, in order",
+		},
+		{
+			code: dedent`
+      // $ExpectType string | number
+      const t = 6 as number | string;
+    `,
+			filename,
+			name: "Union of primitive types, out of order",
+		},
+		{
+			code: dedent`
+      // $ExpectType string[]
+      const t = [] as string[]
+    `,
+			filename,
+			name: "readonly array",
+		},
+		{
+			code: dedent`
+      // $ExpectType readonly string[]
+      const t = [] as readonly string[]
+    `,
+			filename,
+			name: "readonly array",
+		},
+		{
+			code: dedent`
+      // $ExpectType readonly string[]
+      const t = [] as ReadonlyArray<string>
+    `,
+			filename,
+			name: "readonly array and ReadonlyArray",
+		},
+		{
+			code: dedent`
+      // $ExpectType ReadonlyArray<string>
+      const t = [] as readonly string[]
+    `,
+			filename,
+			name: "ReadonlyArray and readonly array",
+		},
+		{
+			code: dedent`
+      // $ExpectType ReadonlyArray<string>
+      const t = [] as ReadonlyArray<string>
+    `,
+			filename,
+			name: "ReadonlyArray",
+		},
 		{
 			code: dedent`
       // $ExpectType { a: number; b: "on"; }
       const t = { a: 17, b: 'on' as const };
     `,
 			filename,
+			name: "Complex type",
 		},
-		// Ignored TypeScript compiler complaints
 		{
 			code: dedent`
       function hasUnusedParam(unusedParam: number, implicitAnyParam) {
@@ -89,6 +152,7 @@ ruleTester.run("expect", expect, {
       }
     `,
 			filename,
+			name: "Ignored TypeScript compiler complaints",
 		},
 		{
 			code: dedent`
@@ -99,7 +163,14 @@ ruleTester.run("expect", expect, {
 		},
 		{
 			code: dedent`
-      // $ExpectType string || number
+      // $ExpectType number || string | number
+      const t = 6 as number;
+      `,
+			filename,
+		},
+		{
+			code: dedent`
+      // $ExpectType string | number || number
       const t = 6 as number;
       `,
 			filename,
