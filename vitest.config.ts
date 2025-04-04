1216486d746b0ca23d8d6c3643e65f2f8a6a8919
Josh Goldberg <git@joshuakgoldberg.com>
2025-04-04 10:08:09 -0400

    lil removals

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
 		clearMocks: true,
 		coverage: {
 			all: true,
-			exclude: ["lib", "src/index.ts", "src/rules/index.ts"],
+			exclude: ["src/index.ts", "src/rules/index.ts"],
 			include: ["src"],
 			reporter: ["html", "lcov"],
 		},
